9013d57a3c39777e7bbbfb26d76e97bcfbc58c48
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-06-15 20:45:17 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,120 @@
+const { ethers } = require("ethers");
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    document.getElementById("connectButton").innerHTML = "Connected";
+  }
+}
+
+async function store() {
+  if (typeof window.ethereum !== "undefined") {
+    const contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+    const abi = [
+      {
+        inputs: [
+          {
+            internalType: "string",
+            name: "_name",
+            type: "string",
+          },
+          {
+            internalType: "uint256",
+            name: "_favoriteNumber",
+            type: "uint256",
+          },
+        ],
+        name: "addPerson",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "string",
+            name: "",
+            type: "string",
+          },
+        ],
+        name: "nameToFavoriteNumber",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        name: "people",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "favoriteNumber",
+            type: "uint256",
+          },
+          {
+            internalType: "string",
+            name: "name",
+            type: "string",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "retrieve",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "uint256",
+            name: "_favoriteNumber",
+            type: "uint256",
+          },
+        ],
+        name: "store",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+    ];
+    //connection to the blockchanin through metamask
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contract_address, abi, signer);
+
+    try {
+      await contract.store(77);
+      document.getElementById("storeButton").innerHTML = "Has been Clicked";
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = {
+  connect,
+  store,
+};
