2c58d9c07e92a623503c8cfe115795348cca567b
Lemmynjash <lemmynjariakiarie@gmail.com>
2022-06-15 22:25:07 +0300

    I solved the errror from this stackoverflow solution
    https://ethereum.stackexchange.com/questions/89879/error-ethjs-query-while-formatting-outputs-from-rpc-messageinvalid-sende

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ async function store() {
   if (typeof window.ethereum !== "undefined") {
     const contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
-    
     const abi = [
       {
         inputs: [
@@ -115,7 +114,107 @@ async function store() {
   }
 }
 
+async function readFromBlockchain() {
+  const contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
+  const abi = [
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "_name",
+          type: "string",
+        },
+        {
+          internalType: "uint256",
+          name: "_favoriteNumber",
+          type: "uint256",
+        },
+      ],
+      name: "addPerson",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "string",
+          name: "",
+          type: "string",
+        },
+      ],
+      name: "nameToFavoriteNumber",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "people",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "favoriteNumber",
+          type: "uint256",
+        },
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "retrieve",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "_favoriteNumber",
+          type: "uint256",
+        },
+      ],
+      name: "store",
+      outputs: [],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+  ];
+  //connection to the blockchanin through metamask
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(contract_address, abi, signer);
+  const value = await contract.retrieve();
+  console.log(value.toString());
+}
 module.exports = {
   connect,
   store,
+  readFromBlockchain,
 };
